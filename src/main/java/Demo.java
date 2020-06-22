57525f134628c99508240ce4e43d5da5a4faf566
shiyanglan <623897451@qq.com>
2020-06-22 16:37:09 +0800

    nizaigm

---
+++ src/main/java/Demo.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: Demo
+ * Author: yanglan88
+ * Date: 2020/6/22 16:34
+ * History:
+ * <author> <time> <version>
+ * 作者姓名 修改时间 版本号 描述
+ *
+ * @author yanglan88
+ * @create 2020/6/22
+ * @since 1.0.0
+ */
+
+
+/**
+ * @author yanglan88
+ * @create 2020/6/22
+ * @since 1.0.0
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("zaiganma");
+    }
+}
+
